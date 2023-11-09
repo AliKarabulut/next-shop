8abb0b6a49fd26af2b05cdc4e91767cad75cee46
Ali Karabulut <a.kbt6666@gmail.com>
2023-11-09 11:00:12 +0300

    Remove unused code and update component props.

--- src/components/admin/left-menu/index.tsx
+++ src/components/admin/left-menu/index.tsx
@@ -3,18 +3,11 @@ import IconButton from "./icon-button";
 
 import BasketIcon from "@/icons/admin/basket";
 import MailIcon from "@/icons/admin/mail";
-interface MenuItem {
-  title?: string;
-  subMenuItems?: MenuItem[];
-}
 
-interface LeftMenuProps {
-  menuItems: MenuItem[];
-}
 
 const list: string[] = ["Products", "Product Details", "Add Product", "Edit Product"];
 
-const LeftMenu: React.FC<LeftMenuProps> = () => {
+const LeftMenu: React.FC = () => {
   return (
     <div className="w-64 px-4 py-6">
       <IconButton text="Order">
