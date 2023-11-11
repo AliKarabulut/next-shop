b9324f63fa441983130432b0482175f337f26039
Ali Karabulut <a.kbt6666@gmail.com>
2023-11-11 18:54:20 +0300

    Add isNarrowed state to Header component and hide menu button on small screens

--- src/components/admin/header/index.tsx
+++ src/components/admin/header/index.tsx
@@ -1,14 +1,16 @@
 "use client";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { narrowedAction } from "@/store/admin/isNarrowed";
 import MenuIcon from "@/icons/admin/menu";
 import SettingsIcon from "@/icons/admin/settings";
 import IconButton from "@/ui/icon-button";
+import { useEffect } from "react";
 
 interface Props {}
 
 const Header: React.FC<Props> = () => {
   const dispatch = useDispatch();
+  const isNarrowed = useSelector((state: any) => state.isNarrowed.isNarrowed);
 
   const toggleExpanded = () => {
     dispatch(narrowedAction.toggle());
@@ -18,7 +20,7 @@ const Header: React.FC<Props> = () => {
     <header className="h-20 px-6 py-4 bg-white flex justify-between items-center">
       <div className="flex items-center">
         <h1 className="w-48">Admin</h1>
-        <IconButton onClick={toggleExpanded}>
+        <IconButton onClick={toggleExpanded} className="max-md:invisible">
           <MenuIcon />
         </IconButton>
       </div>
--- src/ui/icon-button/index.tsx
+++ src/ui/icon-button/index.tsx
@@ -1,11 +1,15 @@
 type IconButtonProps = {
   children: React.ReactNode;
   onClick?: () => void;
+  className?: string;
 };
 
-const IconButton: React.FC<IconButtonProps> = ({ children, onClick }) => {
+const IconButton: React.FC<IconButtonProps> = ({ children, onClick, className }) => {
   return (
-    <div onClick={onClick} className="w-[34px] h-[34px] duration-300 flex items-center justify-center cursor-pointer rounded-lg text-admin-secondary-dark hover:text-admin-secondary-light bg-admin-secondary-light hover:bg-admin-secondary-main">
+    <div
+      onClick={onClick}
+      className={`w-[34px] h-[34px] duration-300 flex items-center justify-center cursor-pointer rounded-lg text-admin-secondary-dark hover:text-admin-secondary-light bg-admin-secondary-light hover:bg-admin-secondary-main ${className}`}
+    >
       {children}
     </div>
   );
