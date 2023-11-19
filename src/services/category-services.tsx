7c50d7198858c05cf547132b8dfffaa69f35edc3
Ali Karabulut <a.kbt6666@gmail.com>
2023-11-20 02:08:37 +0300

    Remove axios dependency and update API calls

--- src/services/category-services.tsx
+++ src/services/category-services.tsx
@@ -1,16 +1,28 @@
 import axios from "axios";
 
 export const getCategories = async (): Promise<any> => {
-  const response = await axios.get(`${process.env.SITE_URL}/api/product/get-category`);
-  return response.data;
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/product/get-category`);
+  return response.json();
 };
 
 export const addCategory = async (name: string): Promise<any> => {
-  const response = await axios.post(`${process.env.SITE_URL}/api/product/create-category`, { name });
-  return response.data;
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/product/create-category`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: name,
+  });
+  return response.json();
 };
 
 export const getVariatonOptions = async (id: string): Promise<any> => {
-  const response = await axios.get(`${process.env.SITE_URL}/api/product/get-variation-options`, { params: { id } });
-  return response.data;
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/product/get-variation-options`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: id,
+  });
+  return response.json();
 };
--- src/services/slider-services.tsx
+++ src/services/slider-services.tsx
@@ -1,11 +1,17 @@
-import axios from "axios";
+import { METHODS } from "http";
 
 export const getSliders = async (): Promise<any> => {
-  const response = await axios.get(`${process.env.SITE_URL}/api/product/slider`);
-  return response.data;
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/product/slider`);
+  return response.json();
 };
 
 export const addSlider = async (data: FormData): Promise<any> => {
-  const response = await axios.post(`${process.env.SITE_URL}/api/product/slider`, data);
-  return response.data;
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/product/slider`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: data,
+  });
+  return response.json();
 };
