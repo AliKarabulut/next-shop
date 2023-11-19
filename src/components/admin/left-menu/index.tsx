d5d6215a093876aaaaea8c7f988ab64a43810289
Ali Karabulut <a.kbt6666@gmail.com>
2023-11-20 00:24:08 +0300

    Add HandMoveIcon component to LeftMenu

--- src/components/admin/left-menu/index.tsx
+++ src/components/admin/left-menu/index.tsx
@@ -1,21 +1,16 @@
-"use client"
+"use client";
 import { useDispatch, useSelector } from "react-redux";
 import { narrowedAction } from "@/store/admin/isNarrowed";
 import TruckDeliveryIcon from "@/icons/admin/truck";
 import BasketIcon from "@/icons/admin/basket";
 import MailIcon from "@/icons/admin/mail";
 import Button from "@/ui/button";
+import HandMoveIcon from "@/icons/admin/hand-move";
 
 const list = [
   { item: "New Prouct", href: "new-product" },
   { item: "Edit Product", href: "edit-product" },
   { item: "Products", href: "products" },
-];
-
-const list2 = [
-  { item: "New Prouct", href: "new-products" },
-  { item: "Edit Product", href: "edit-products" },
-  { item: "Products", href: "productss" },
 ];
 
 const LeftMenu: React.FC = () => {
@@ -24,9 +19,12 @@ const LeftMenu: React.FC = () => {
   const dispatch = useDispatch();
 
   return (
-    <nav className={`flex-shrink-0 px-4 py-6 bg-white h-[calc(100vh-80px)] max-sm:absolute z-10 top-20 left-0 duration-300 ${isMobile ? "max-sm:translate-x-0" : "max-sm:-translate-x-72 "} `}
-    onMouseEnter={() => isClicked && dispatch(narrowedAction.onNarrowed())}
-    onMouseLeave={() => isClicked && dispatch(narrowedAction.offNarrowed())}
+    <nav
+      className={`flex-shrink-0 px-4 py-6 bg-white h-[calc(100vh-80px)] max-sm:absolute z-10 top-20 left-0 duration-300 ${
+        isMobile ? "max-sm:translate-x-0" : "max-sm:-translate-x-72 "
+      } `}
+      onMouseEnter={() => isClicked && dispatch(narrowedAction.onNarrowed())}
+      onMouseLeave={() => isClicked && dispatch(narrowedAction.offNarrowed())}
     >
       <Button text="Order" href="order">
         <TruckDeliveryIcon className="flex-shrink-0" />
@@ -37,8 +35,8 @@ const LeftMenu: React.FC = () => {
       <Button text="Mail" href="mail">
         <MailIcon className="flex-shrink-0" />
       </Button>
-      <Button text="E-commerce" subMenuItems={list2}>
-        <BasketIcon className="flex-shrink-0" />
+      <Button text="Slider" href="slider">
+        <HandMoveIcon className="flex-shrink-0"/>
       </Button>
     </nav>
   );
