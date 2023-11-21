4cb7458da76c6bb4892bed5bbd021b52b06023e1
Ali Karabulut <a.kbt6666@gmail.com>
2023-11-22 01:07:10 +0300

    Update Quill toolbar options

--- src/components/admin/react-quill/index.tsx
+++ src/components/admin/react-quill/index.tsx
@@ -13,16 +13,10 @@ const Quill = () => {
     () => ({
       toolbar: [
         ["bold", "italic", "underline", "strike"],
-        ["blockquote", "code-block"],
-        [{ header: 1 }, { header: 2 }],
-        [{ list: "ordered" }, { list: "bullet" }],
-        [{ script: "sub" }, { script: "super" }],
-        [{ indent: "-1" }, { indent: "+1" }],
-        [{ direction: "rtl" }],
         [{ size: ["small", false, "large", "huge"] }],
-        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        [{ header: 1 }, { header: 2 }],
+        [{ script: "sub" }, { script: "super" }],
         [{ color: [] }, { background: [] }],
-        [{ align: [] }],
       ],
     }),
     []
