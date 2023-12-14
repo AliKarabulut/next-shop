01b817f14cc951bd41769b7d3aaba986e1b304ab
Ali Karabulut <a.kbt6666@gmail.com>
2023-12-15 01:31:53 +0300

    slider style fix

--- src/components/swipers/intro-swiper/index.tsx
+++ src/components/swipers/intro-swiper/index.tsx
@@ -27,15 +27,21 @@ const IntroSwiper = ({ slides }: SwiperProps) => {
       slidesPerView={1}
       pagination={{ clickable: true }}
       navigation
-      className="h-96 py-2 bg-gradient-to-r from-grayLighter to-grayLight"
+      className="h-[26.25rem] py-2 bg-gradient-to-r from-grayLighter to-grayLight"
     >
       {slides?.map((slide, index) => (
-        <SwiperSlide key={index}>
-          <div className="grid grid-cols-12 items-center xl:px-32 container mx-auto w-full">
-            <div className="col-span-6 sm:col-span-8" dangerouslySetInnerHTML={{ __html: slide.description }} />
-            <div className="col-span-6 sm:col-span-4 ml-auto h-96">
+        <SwiperSlide key={index} className="h-full">
+          <div className="flex items-center justify-between !pr-0 xl:!px-32 container mx-auto w-full h-full">
+            <div className="w-1/2 sm:w-2/3" dangerouslySetInnerHTML={{ __html: slide.description }} />
+            <div className="ml-auto h-full max-h-[26.25rem]">
               {slide.image.urls[0] && (
-                <Image src={slide.image.urls[0]} alt={`Slide image ${index}`} width={520} height={380} className="object-contain h-full w-fit" />
+                <Image
+                  src={slide.image.urls[0]}
+                  alt={`Slide image ${index}`}
+                  width={520}
+                  height={380}
+                  className="object-contain h-full w-fit"
+                />
               )}
             </div>
           </div>
