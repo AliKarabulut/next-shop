3dd582bc9922b4990ddc817cbda53c39bcd455cc
Ali Karabulut <a.kbt6666@gmail.com>
2023-12-15 00:16:47 +0300

    hıver fix actionbutton

--- src/components/header/actions/actionButton.tsx
+++ src/components/header/actions/actionButton.tsx
@@ -10,7 +10,7 @@ type ActionIconProps = {
 
 const ActionIcon = ({ children, quantity, price, className, dropDown }: ActionIconProps) => {
   return (
-    <div className={`actionIcon group relative cursor-pointer duration-300 flex gap-2 items-center ${className}`}>
+    <div className={`actionIcon group relative cursor-pointer duration-300 flex gap-2 items-center w-fit ${className}`}>
       {children}
       {quantity && (
         <div
@@ -24,13 +24,14 @@ const ActionIcon = ({ children, quantity, price, className, dropDown }: ActionIc
       {dropDown && (
         <div
           className="hidden group-hover:block border-t-2 border-t-yellow border rounded-b-lg
-        text-lightDark  border-grayLighter drop-shadow-lg w-fit bg-white 
-          absolute top-8 -left-9 before:content-[''] before:absolute
-          before:-top-2 before:left-1/2 before:-translate-x-1/2 
-          before:border-x-[6px] before:border-x-transparent 
-          before:border-b-[6px] before:border-b-yellow opacity-0 animate-opacityFast
-          after:content-[''] after:absolute after:-top-4 after:w-full after:h-4 
-          after:left-1/2 after:-translate-x-1/2 z-50"
+ text-lightDark  border-grayLighter drop-shadow-lg w-fit bg-white 
+   absolute top-7 left-1/2 transform -translate-x-1/2 
+   before:content-[''] before:absolute
+   before:-top-2 before:left-1/2 before:-translate-x-1/2 
+   before:border-x-[6px] before:border-x-transparent 
+   before:border-b-[6px] before:border-b-yellow opacity-0 animate-opacityFast
+   after:content-[''] after:absolute after:-top-4 after:w-full after:h-4 
+   after:left-1/2 after:-translate-x-1/2 z-50"
         >
           {dropDown.map((item, index) => (
             <Link href={item.link} key={index} className="text-sm block border-b border-yellow last:border-none px-4 py-2 hover:bg-grayLight">
--- src/components/header/category/categoryItem.tsx
+++ src/components/header/category/categoryItem.tsx
@@ -9,7 +9,7 @@ type CategoryItemProps = {
 export const CategoryItem = ({ name, href, icon }: CategoryItemProps) => {
   return (
     <Link href={href} className="font-light text-sm py-1 px-4 flex items-center w-fit">
-      <span className="shrink-0 mr-2">{icon}</span>
+      <span className="shrink-0 mr-2.5">{icon}</span>
       <span>{name}</span>
     </Link>
   );
