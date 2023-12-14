08c07a8025130cb21766c2991463a85975705c95
Ali Karabulut <a.kbt6666@gmail.com>
2023-12-15 01:52:12 +0300

    fix special  offer style

--- src/components/cards/specialOffer/index.tsx
+++ src/components/cards/specialOffer/index.tsx
@@ -20,7 +20,7 @@ type SpecialOfferProps = {
 const SpecialOffer = ({ className, production }: SpecialOfferProps) => {
   return (
     <div
-      className={`w-full rounded-3xl p-5 border-2 border-yellow relative ${className} flex flex-col items-center justify-evenly max-w-md text-lightDark`}
+      className={`w-full rounded-3xl p-5 border-2 border-yellow relative ${className} flex flex-col items-center justify-between max-w-md text-lightDark`}
     >
       <div className="flex justify-between w-full items-center">
         <span className="text-xl text-left w-full">Special Offer</span>
@@ -30,12 +30,14 @@ const SpecialOffer = ({ className, production }: SpecialOfferProps) => {
         </div>
       </div>
       <Image src={production.image.src} width={400} height={400} alt={production.name} className="object-contain max-h-96 h-fit" />
-      <Link href={production.url} className="text-inherit text-lightDark hover:text-yellow font-bold text-sm">
-        {production.name}
-      </Link>
-      <div className="flex gap-4 items-center">
-        <span className="text-redDark text-3xl">${production.price}</span>
-        <span className="text-lg text-grayDarker line-through">${+production.price - production.discount}</span>
+      <div className="flex flex-col items-center gap-3">
+        <Link href={production.url} className="text-inherit text-lightDark hover:text-yellow font-bold text-sm">
+          {production.name}
+        </Link>
+        <div className="flex gap-4 items-center">
+          <span className="text-redDark text-3xl">${production.price}</span>
+          <span className="text-lg text-grayDarker line-through">${+production.price - production.discount}</span>
+        </div>
       </div>
       <ProgressBar max={60} value={6} />
       <HurryUp date={1702767824276} />
