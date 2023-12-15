5e0492cf707bc3e8f6579f17d82f2b256427db8a
Ali Karabulut <a.kbt6666@gmail.com>
2023-12-16 00:03:47 +0300

    grid system  home page

--- src/app/(e-commerce)/page.tsx
+++ src/app/(e-commerce)/page.tsx
@@ -1,44 +1,44 @@
-import ProductCard from "@/components/cards/productCard";
-import SpecialOffer from "@/components/cards/specialOffer";
-import IntroSwiper from "@/components/swipers/intro-swiper";
-import prisma from "@/libs/prismadb";
+import ProductCard from '@/components/cards/productCard'
+import SpecialOffer from '@/components/cards/specialOffer'
+import IntroSwiper from '@/components/swipers/intro-swiper'
+import prisma from '@/libs/prismadb'
 
 const getData = async () => {
   const sliders = await prisma.slider.findMany({
     include: { image: true },
-  });
-  return sliders;
-};
+  })
+  return sliders
+}
 
 const production = {
-  name: "Xbox Gamepad",
-  price: "100.00",
+  name: 'Xbox Gamepad',
+  price: '100.00',
   discount: 10,
-  url: "/",
+  url: '/',
   image: {
-    src: "/products/xboxgamepad.png",
-    name: "Xbox Gamepad",
+    src: '/products/xboxgamepad.png',
+    name: 'Xbox Gamepad',
   },
-};
+}
 
 const Home = async () => {
-  const sliders = await getData();
+  const sliders = await getData()
 
   return (
     <main>
       <IntroSwiper slides={sliders} />
       <section>
-        <div className="container grid grid-cols-6 sm:grid-cols-8 lg:grid-cols-10 xl:grid-cols-12  gap-8 mx-auto mt-20">
-          <SpecialOffer production={production} className="col-span-6 lg:col-span-4 row-span-2" />
+        <div className="container mx-auto mt-20 grid grid-cols-4 gap-6 sm:grid-cols-6 md:gap-8 lg:grid-cols-8 xl:grid-cols-10 2xl:grid-cols-12">
+          <SpecialOffer production={production} className="col-span-4 row-span-2 mx-auto sm:mx-0" />
           {Array(10)
             .fill(0)
             .map((_, index) => (
-              <ProductCard key={index} className="col-span-2"/>
+              <ProductCard key={index} className="col-span-2" />
             ))}
         </div>
       </section>
     </main>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
--- src/components/cards/specialOffer/index.tsx
+++ src/components/cards/specialOffer/index.tsx
@@ -1,48 +1,47 @@
-import HurryUp from "@/components/production-components/hurry-up";
-import ProgressBar from "@/ui/progress-bar";
-import Image from "next/image";
-import Link from "next/link";
+import HurryUp from '@/components/production-components/hurry-up'
+import ProgressBar from '@/ui/progress-bar'
+import Image from 'next/image'
+import Link from 'next/link'
 
 type SpecialOfferProps = {
-  className?: string;
+  className?: string
   production: {
-    name: string;
-    price: string;
-    discount: number;
-    url: string;
+    name: string
+    price: string
+    discount: number
+    url: string
     image: {
-      src: string;
-      name: string;
-    };
-  };
-};
+      src: string
+      name: string
+    }
+  }
+}
 
 const SpecialOffer = ({ className, production }: SpecialOfferProps) => {
   return (
     <div
-      className={`w-full rounded-3xl p-5 border-2 border-yellow relative ${className} flex flex-col items-center justify-between max-w-md text-lightDark`}
-    >
-      <div className="flex justify-between w-full items-center">
-        <span className="text-xl text-left w-full">Special Offer</span>
-        <div className="w-20 h-20 shrink-0 bg-yellow rounded-full flex justify-center items-center flex-col">
+      className={`relative w-full rounded-3xl border-2 border-yellow p-5 ${className} flex max-w-md flex-col items-center justify-between text-lightDark`}>
+      <div className="flex w-full items-center justify-between">
+        <span className="w-full text-left text-xl">Special Offer</span>
+        <div className="flex h-20 w-20 shrink-0 flex-col items-center justify-center rounded-full bg-yellow">
           <span className="text-xs">Save</span>
           <span className="text-xl font-bold">{production.discount}%</span>
         </div>
       </div>
-      <Image src={production.image.src} width={400} height={400} alt={production.name} className="object-contain max-h-96 h-fit" />
+      <Image src={production.image.src} width={400} height={400} alt={production.name} className="h-fit max-h-96 object-contain" />
       <div className="flex flex-col items-center gap-3">
-        <Link href={production.url} className="text-inherit text-lightDark hover:text-yellow font-bold text-sm">
+        <Link href={production.url} className="text-sm font-bold text-inherit text-lightDark hover:text-yellow">
           {production.name}
         </Link>
-        <div className="flex gap-4 items-center">
-          <span className="text-redDark text-3xl">${production.price}</span>
+        <div className="flex items-center gap-4">
+          <span className="text-3xl text-redDark">${production.price}</span>
           <span className="text-lg text-grayDarker line-through">${+production.price - production.discount}</span>
         </div>
       </div>
       <ProgressBar max={60} value={6} />
       <HurryUp date={1702767824276} />
     </div>
-  );
-};
+  )
+}
 
-export default SpecialOffer;
+export default SpecialOffer
