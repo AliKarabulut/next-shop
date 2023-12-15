3bf652f45a8e508c3212d737dd619a6410aa10bb
Ali Karabulut <a.kbt6666@gmail.com>
2023-12-16 01:40:43 +0300

    footer v1

--- src/app/(e-commerce)/layout.tsx
+++ src/app/(e-commerce)/layout.tsx
@@ -1,22 +1,24 @@
-import Header from "@/components/header";
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import Header from '@/components/header'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Nunito_Sans } from 'next/font/google'
+import Footer from '@/components/footer'
 
-const inter = Inter({ weight: ["100", "300", "400", "500", "700", "900"], subsets: ["latin"] });
+const nunito = Nunito_Sans({ weight: ['200', '300', '400', '500', '700', '900'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "E-commerce",
-  description: "E-commerce",
-};
+  title: 'E-commerce',
+  description: 'E-commerce',
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={nunito.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
-  );
+  )
 }
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,63 @@
+import { TbMapPin, TbPhone, TbMail } from 'react-icons/tb'
+
+const Footer = () => {
+  return (
+    <section className="bg-grayLighter">
+      <div className="container mx-auto py-16 text-sm">
+        <div className="mb-6 grid grid-cols-6 items-center gap-8 md:mb-9 lg:grid-cols-12">
+          <h4 className="col-span-3 text-3xl lg:col-span-9">E-commerce</h4>
+          <div>İcons</div>
+        </div>
+        <div className="grid grid-cols-6 gap-8 lg:grid-cols-12">
+          <address className="col-span-3 not-italic">
+            <ul className="flex flex-col gap-2">
+              <li className="flex items-center gap-3">
+                <TbMapPin className="shrink-0 text-xl" aria-hidden="true" /> Home Office
+              </li>
+              <li className="flex items-center gap-3">
+                <TbPhone className="shrink-0 text-xl" aria-hidden="true" /> <a href="tel:05418632292">0541 863 29 92</a>
+              </li>
+              <li className="flex items-center gap-3">
+                <TbMail className="shrink-0 text-xl" aria-hidden="true" /> <a href="mailto:info@trendbox.io">karabulut.ali@hotmail.com</a>
+              </li>
+            </ul>
+          </address>
+          <div className="col-span-3">
+            <ul className="flex flex-col gap-2">
+              <li>Privacy Policy</li>
+              <li>Terms & Conditions</li>
+              <li>Returns & Exchanges</li>
+              <li>Shipping & Delivery</li>
+            </ul>
+          </div>
+          <div className="col-span-3">
+            <ul className="flex flex-col gap-2">
+              <li>Store Location</li>
+              <li>FAQs</li>
+              <li>Orders Tracking</li>
+            </ul>
+          </div>
+          <div className="col-span-3">
+            <ul className="flex flex-col gap-2">
+              <li className="flex w-full justify-between border-b border-grayLight">
+                <span>Monday - Friday</span>
+                <span>08:00 - 20:00</span>
+              </li>
+              <li className="flex w-full justify-between border-b border-grayLight">
+                <span>Saturday</span>
+                <span>09:00 - 21:00</span>
+              </li>
+              <li className="flex w-full justify-between border-b border-grayLight">
+                <span>Sunday</span>
+                <span>13:00 - 21:00</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="w-full bg-white py-5 text-center text-sm leading-6">2023 Created by Ali Karabulut. Front End Developer</div>
+    </section>
+  )
+}
+
+export default Footer
