a1829e8bfdd938e753bb928d5117b2844b00b5f8
Ali Karabulut <a.kbt6666@gmail.com>
2024-01-07 19:19:44 +0300

    Refactor Input component to use single quotes and add onChangeProps type

--- src/ui/input/input.tsx
+++ src/ui/input/input.tsx
@@ -1,51 +1,57 @@
-"use client";
-import { useState, useEffect, useRef } from "react";
+'use client'
+import { useState, useEffect, useRef } from 'react'
+
+type onChangeProps = {
+  name: string
+  data: string
+}
 
 type InputProps = {
-  type?: "number" | "text" | "password" | "email";
-  label: string;
-  name: string;
-  capitalize?: boolean;
-  inputClassName?: string;
-  labelClassName?: string;
-  value?: string;
-  onChange?: (e: any) => void;
-};
+  type?: 'number' | 'text' | 'password' | 'email'
+  label: string
+  name: string
+  capitalize?: boolean
+  inputClassName?: string
+  labelClassName?: string
+  value?: string
+  onChange?: (e: onChangeProps) => void
+}
 
-const Input = ({ label, name, onChange, capitalize = true, inputClassName, labelClassName, value, type = "text" }: InputProps) => {
-  const [open, setOpen] = useState<boolean>(false);
-  const [inputValue, setInputValue] = useState<string>("");
-  const inputRef = useRef<HTMLDivElement>(null);
+const Input = ({ label, name, onChange, capitalize = true, inputClassName, labelClassName, value, type = 'text' }: InputProps) => {
+  const [open, setOpen] = useState<boolean>(false)
+  const [inputValue, setInputValue] = useState<string>('')
+  const inputRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
       if (inputRef.current && !inputRef?.current.contains(event.target as Node)) {
-        setOpen(false);
+        setOpen(false)
       }
     }
 
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener('mousedown', handleClickOutside)
 
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
 
-  const clickHandler = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    setOpen(true);
-  };
+  const clickHandler = () => {
+    setOpen(true)
+  }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-    if (onChange) onChange({ name: name, data: e.target.value });
-  };
+    setInputValue(e.target.value)
+    if (onChange) onChange({ name: name, data: e.target.value })
+  }
 
   return (
     <div className="relative" ref={inputRef}>
       <label
         htmlFor={name}
-        className={`absolute text-admin-grey-500 transition-all ${open || inputValue ? "-top-5 text-xs " : "pl-3 text-base top-2"} ${labelClassName}`}
-      >
+        className={`absolute text-admin-grey-500 transition-all ${
+          open || inputValue ? '-top-5 text-xs ' : 'top-2 pl-3 text-base'
+        } ${labelClassName}`}>
         {label}
       </label>
       <input
@@ -55,14 +61,14 @@ const Input = ({ label, name, onChange, capitalize = true, inputClassName, label
         id={name}
         onChange={handleChange}
         onClick={clickHandler}
-        className={`rounded-xl w-full px-3 py-2 outline-none hover:shadow-md shadow-admin-secondary-dark transition-all text-base focus:shadow-md ${
-          open ? "cursor-text" : "cursor-pointer"
-        } ${capitalize ? "capitalize" : ""} ${inputClassName}`}
-        style={{ caretColor: "#697586" }}
+        className={`w-full rounded-xl px-3 py-2 text-base shadow-admin-secondary-dark outline-none transition-all hover:shadow-md focus:shadow-md ${
+          open ? 'cursor-text' : 'cursor-pointer'
+        } ${capitalize ? 'capitalize' : ''} ${inputClassName}`}
+        style={{ caretColor: '#697586' }}
         autoComplete="off"
       />
     </div>
-  );
-};
+  )
+}
 
-export default Input;
+export default Input
