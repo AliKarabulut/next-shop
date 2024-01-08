a8711f2c52f11a46837e3f9002932dce5d836bd6
alikarabulut <a.kbt6666@gmail.com>
2024-01-08 14:50:19 +0300

    prisma update

--- src/libs/prismadb/index.tsx
+++ src/libs/prismadb/index.tsx
@@ -1,10 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-declare global {
-    var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-const client = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
+const client = globalForPrisma.prisma || new PrismaClient()
 
-export default client;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = client
+
+export default client
