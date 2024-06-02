80e4ca799c8d4344c564dac5f03d7f5e76c9ee99
alikarabulut <a.kbt6666@gmail.com>
2024-06-03 01:45:11 +0300

    refactor: update middleware matcher config to exclude .png files

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,5 +27,5 @@ export default auth(req => {
 })
 
 export const config = {
-  matcher: ['/((?!api|_next/static|/_next/image|.png).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon\\.ico|[^\\.]+\\..+).*)'],
 }
