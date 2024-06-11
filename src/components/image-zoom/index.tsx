32b2414d5d459d5ac692a6ee6665e87b9cafa8f3
alikarabulut <a.kbt6666@gmail.com>
2024-06-11 04:22:11 +0300

    feat: image zoom component

--- src/components/image-zoom/index.tsx
+++ src/components/image-zoom/index.tsx
@@ -1,26 +1,57 @@
 'use client'
-import { useState } from 'react'
-// import Image from 'next/image'
+import { useRef, useEffect } from 'react'
+import Image from 'next/image'
 
 import cn from '@/utils/cn'
 
 type ImageZoomProps = {
-  imageUrl?: string
+  image: {
+    src: string
+    alt: string
+  }
 }
 
-const ImageZoom: React.FC<ImageZoomProps> = ({ imageUrl }: ImageZoomProps) => {
-  const [position, setPosition] = useState({ left: 0, top: 0 })
+const ImageZoom: React.FC<ImageZoomProps> = ({ image }: ImageZoomProps) => {
+  const baseProductRef = useRef<HTMLDivElement | null>(null)
+  const zoomAreaRef = useRef<HTMLDivElement | null>(null)
+  const zoomedImageRef = useRef<HTMLDivElement | null>(null)
 
-  const handleMouseMove = (event: React.MouseEvent) => {
-    setPosition({ left: event.clientX, top: event.clientY })
-    console.log(event.clientX, event.clientY)
-  }
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      if (baseProductRef.current) {
+        const baseProduct = baseProductRef.current.getBoundingClientRect()
+        const top = Math.max(0, Math.min(baseProduct.height - 192, event.clientY - baseProduct.top - 96))
+        const left = Math.max(0, Math.min(baseProduct.width - 160, event.clientX - baseProduct.left - 80))
+        if (zoomAreaRef.current && zoomedImageRef.current) {
+          zoomAreaRef.current.style.top = `${top}px`
+          zoomAreaRef.current.style.left = `${left}px`
+          zoomedImageRef.current.style.backgroundPositionX = `${-left * 3}px`
+          zoomedImageRef.current.style.backgroundPositionY = `${-top * 3}px`
+        }
+      }
+    }
+
+    document.addEventListener('mousemove', handleMouseMove)
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove)
+    }
+  }, [])
 
   return (
-    <div onMouseMove={handleMouseMove} className="absolute inset-0">
-      {/* <Image /> */}
-      <div className={cn('absolute h-48 w-40 bg-white/40')}>ImageZoom</div>
-      <div style={{ left: position.left, top: position.top }}></div>
+    <div ref={baseProductRef} id="base-product" className="group absolute inset-0">
+      <Image width={400} height={600} className="object-contain" alt={image.alt} src={image.src} />
+      <div
+        ref={zoomAreaRef}
+        id="zoom-area"
+        className={cn(
+          'invisible absolute flex h-48 w-40 cursor-crosshair items-center justify-center bg-white opacity-0 transition-opacity duration-300 group-hover:visible group-hover:opacity-40',
+        )}></div>
+      <div
+        ref={zoomedImageRef}
+        id="zoomed-image"
+        className="bg-ori invisible absolute -inset-8 translate-x-[110%] bg-blue-100 bg-[length:1200px_1800px] bg-no-repeat opacity-0 transition-opacity delay-300 duration-200 group-hover:visible group-hover:opacity-100"
+        style={{ backgroundImage: `url(${image.src})` }}></div>
     </div>
   )
 }
--- src/components/single-product-gallery/base-product/index.tsx
+++ src/components/single-product-gallery/base-product/index.tsx
@@ -1,9 +1,106 @@
+'use client'
+import { Navigation } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { useState } from 'react'
+import Image from 'next/image'
+
 import ImageZoom from '@/components/image-zoom'
 
+const images = [
+  {
+    src: '/products/zoom-1.jpg',
+    alt: 'Image 1',
+  },
+  {
+    src: '/products/zoom-2.jpg',
+    alt: 'Image 2',
+  },
+  {
+    src: '/products/zoom-3.jpg',
+    alt: 'Image 3',
+  },
+  {
+    src: '/products/zoom-1.jpg',
+    alt: 'Image 1',
+  },
+  {
+    src: '/products/zoom-2.jpg',
+    alt: 'Image 2',
+  },
+  {
+    src: '/products/zoom-3.jpg',
+    alt: 'Image 3',
+  },
+  {
+    src: '/products/zoom-1.jpg',
+    alt: 'Image 1',
+  },
+  {
+    src: '/products/zoom-2.jpg',
+    alt: 'Image 2',
+  },
+  {
+    src: '/products/zoom-3.jpg',
+    alt: 'Image 3',
+  },
+  {
+    src: '/products/zoom-1.jpg',
+    alt: 'Image 1',
+  },
+  {
+    src: '/products/zoom-2.jpg',
+    alt: 'Image 2',
+  },
+  {
+    src: '/products/zoom-3.jpg',
+    alt: 'Image 3',
+  },
+]
+
 const BaseProduct = () => {
+  const [image, setImage] = useState(images[0])
+
+  const setZoomImageUrl = (image: { src: string; alt: string }) => {
+    setImage(image)
+  }
   return (
-    <div className="relative h-[600px] w-96 rounded-lg border bg-red-100">
-      <ImageZoom />
+    <div className="base-product-wrapper ml-40 mt-40 w-[400px]">
+      <div className="relative h-[600px] w-full rounded-lg border bg-red-100">
+        <ImageZoom image={image} />
+        <div className="prev-button absolute left-0 top-1/2 size-8 -translate-y-1/2 cursor-pointer rounded-full bg-blue-400"></div>
+        <div className="next-button absolute right-0 top-1/2 size-8 -translate-y-1/2 cursor-pointer rounded-full bg-blue-400"></div>
+      </div>
+      <Swiper
+        modules={[Navigation]}
+        slidesPerView="auto"
+        loop={true}
+        navigation={{
+          nextEl: '.next-button',
+          prevEl: '.prev-button',
+        }}
+        onSlideChange={slide => {
+          const activeImage = images[slide.realIndex]
+          setImage(activeImage)
+        }}
+        className="mt-4">
+        {images?.map((image, index) => (
+          <SwiperSlide key={index}>
+            <figure className="h-full">
+              {
+                <Image
+                  src={image.src}
+                  alt={image.alt ?? `Slide image ${index}`}
+                  width={40}
+                  height={60}
+                  className="h-full object-contain"
+                  onClick={() => setZoomImageUrl(image)}
+                  onMouseOver={() => setZoomImageUrl(image)}
+                />
+              }
+            </figure>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   )
 }
